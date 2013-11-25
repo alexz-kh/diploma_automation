82b74a44f07bde2e3495de6cc3fa326a7f311ea2
azvyagintsev <azvyagintsev@mirantis.com>
2013-11-25 15:44:52 +0200

    add

--- init/init.sh
+++ init/init.sh
@@ -2,11 +2,17 @@
 
 
 
-
+#todo add if!
 yum install -y git
+
 git clone git://github.com/alexz-kh/diploma_automation.git
 
-./diploma_automation/role/next_role.sh
+cd diploma_automation
+git reset
+git stash
+git pull --force
+
+./role/next_role.sh
 
 
 
