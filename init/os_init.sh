ff974c9c2378b149f3861cb5af0b514e60640b51
alexz <alexz.kh.ua@gmail.com>
2013-12-21 16:43:14 +0200

    modified:   os_init.sh

--- init/os_init.sh
+++ init/os_init.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-DIR=`dirname $0`
-cd $DIR
+#!/bin/bash
 
+# os_init      init file for starting installing of Openshift_fiploma
+#
+# chkconfig:   - 90 10
+# description: Starts and stops the MongDB daemon that handles all \
+#              database requests.
+#how-to add me:
 #Put this file on VMs! and one-time run
-#$ echo "/root/bootstrap/os_init.sh" >>  /etc/rc.local
+#chkconfig --add /etc/init.d/os_init.sh 
+#chkconfig --list os_init.sh
+#chkconfig os_init.sh on
 
 if [ -f "/root/bootstrap/finish" ] ; then exit
 
@@ -16,24 +23,29 @@ else
     echo "Git does not exist!installing..."
     yum install -y git
     if [ $? -ne 0 ]; then
-        echo "Error!!" ; exit 1 ; fi
+    echo "Error!!" ; exit 1 ; fi
 
 fi
+
+mkdir -p /root/bootstrap/
+cd /root/bootstrap/
 
 if [ -d "diploma_automation" ]; then
     cd diploma_automation
     git stash
-    git checkout master
-    git pull origin master --force
-    cd ../
-    else
-    git clone git://github.com/alexz-kh/diploma_automation.git
+    git checkout master 
     if [ $? -ne 0 ]; then
-	echo "Error!!" ; exit 1 ; fi
-
+    echo "Error!!" ; exit 1 ; fi
+    git pull origin master --force 
+    if [ $? -ne 0 ]; then
+    echo "Error!!" ; exit 1 ; fi
+    else
+    git clone git://github.com/alexz-kh/diploma_automation.git 
+    if [ $? -ne 0 ]; then
+    echo "Error!!" ; exit 1 ; fi
 fi
 
-./diploma_automation/init/install.sh &>>  /root/bootstrap/os_init_log_`date "+%Y-%m-%d-%H-%M"`
+/root/bootstrap/diploma_automation/init/install.sh &>>  /root/bootstrap/os_init_log_`date "+%Y-%m-%d-%H-%M"` &
 
 fi
 exit
